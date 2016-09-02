81fcbbe5667ad15fb0dd05c9ef7b60738f22910f
Franco Salinas <franco.salinas.mendoza@gmail.com>
2016-09-01 22:54:46 -0300

    Concat and minify CSS files.

--- gulpfile.js
+++ gulpfile.js
@@ -7,6 +7,8 @@ var uglify       = require('gulp-uglify');
 var copy         = require('gulp-copy');
 var runSequence  = require('run-sequence');
 var browserSync  = require('browser-sync');
+var cleanCSS     = require('gulp-clean-css');
+var merge        = require('merge-stream');
 
 gulp.task('js', function(){
   return gulp.src([
@@ -17,7 +19,7 @@ gulp.task('js', function(){
     './bower_components/angular-translate/angular-translate.min.js',
     './bower_components/angular-translate-loader-static-files/angular-translate-loader-static-files.min.js',
     './bower_components/angular-slugify/angular-slugify.js',
-    
+
     './app/app.js'])
     .pipe(concat('app.min.js'))
     .pipe(uglify())
@@ -31,7 +33,7 @@ gulp.task('serve', function() {
       baseDir: "./"
     }
   });
-  
+
   gulp.watch("./less/default/*.less", ['less']);
 
   gulp.watch("./*.html")
@@ -43,28 +45,36 @@ gulp.task('serve', function() {
   .on('change', browserSync.reload);
 });
 
-gulp.task('less', [], function() {
-  return gulp.src('./less/default/styles.less')
+gulp.task('css', [], function() {
+  var lessStream = gulp.src([
+    './less/default/styles.less'
+  ])
   .pipe(less())
+  .pipe(concat('less-files.less'));
+
+  var cssStream = gulp.src([
+    './bower_components/bootstrap-css-only/css/bootstrap.min.css',
+    './bower_components/components-font-awesome/css/font-awesome.min.css'
+  ])
+  .pipe(concat('css-files.css'));
+
+  var mergedStream = merge(lessStream, cssStream)
+  .pipe(concat('styles.css'))
+  .pipe(cleanCSS({compatibility: 'ie8'}))
   .pipe(gulp.dest('./stylesheets'))
   .on('end', browserSync.reload);
+
+  return mergedStream;
 });
 
 gulp.task('setup', [], function() {
   /* Set up awesome font. */
   gulp.src('./bower_components/components-font-awesome/fonts/*.*')
   .pipe(gulp.dest('./fonts'));
-
-  gulp.src('./bower_components/components-font-awesome/css/font-awesome.min.css')
-  .pipe(gulp.dest('./stylesheets'));
-
-  /* Set up bootstrap css.  */
-  gulp.src('./bower_components/bootstrap-css-only/css/bootstrap.min.css')
-  .pipe(gulp.dest('./stylesheets'));
 })
 
 gulp.task('build', [], function() {
-  runSequence('js', 'less');
+  runSequence('js', 'css');
 });
 
 gulp.task('default', ['build'], function() {});
